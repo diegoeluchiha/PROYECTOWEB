9dc23b283a0e1533b9c1d3e7ec26b93400f52288
Diego Ubilla <diegoubillaramos@gmail.com>
2022-06-23 12:39:02 -0400

    modal eliminar usuario terminado

--- app/Views/dashboard/user/index.php
+++ app/Views/dashboard/user/index.php
@@ -17,25 +17,60 @@
                 <td><?=$u->username ?></td>
                 <td>
                     <a class=" ml-2 btn btn-dark btn-sm"  data-toggle="tooltip" data-placement="top" title="Editar" href="<?= base_url() ?>/cliente/<?=$u->id ?>/edit"><i class="bi bi-pencil"></i></a>
-                    <a class=" ml-2 btn btn-danger btn-sm"  data-toggle="tooltip" data-placement="top" title="Eliminar" href="<?= base_url() ?>/cliente/delete/<?=$u->id ?>" onclick="return confirmdelete();"><i class="bi bi-trash3"></i></a>
+                    <a class=" ml-2 btn btn-danger btn-sm" data-bs-id="<?=$u->id ;?>" data-bs-toggle="modal" data-bs-target="#eliminarModalusuario"  data-toggle="tooltip" data-placement="top" title="Eliminar" ><i class="bi bi-trash3"></i></a>
                 </td>
             </tr>
         <?php endforeach?>
 
-
-        <!-- <script>
-            function isconfirm(){
-
-            if(!confirm('¿Esta seguro que desea eliminar la siguiente fila.?')){
-            event.preventDefault();
-            return;
-            }
-            return true;
-            }
-        </script> -->
     </tbody>
  
 </table>
 <!-- pagers link -->
 
 </div>
+
+<!-- modal eliminar -->
+<div class="modal fade" id="eliminarModalusuario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header bg-dark text-light ">
+        <h5 class="modal-title mx-auto">Mensaje</h5>
+        <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+      </div>
+      <div class="modal-body text-center">
+        <b>   ¿Seguro que deseas eliminar el registro?</b>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cerrar</button>
+        
+        <a id="deleteform" data-bs-action="<?=base_url()?>/cliente/delete/" href="" type="button" class="btn btn-danger">Eliminar</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- modal eliminar -->
+
+
+<!-- escript -->
+<script>
+  var eliminarModal = document.getElementById('eliminarModalusuario')
+  eliminarModal.addEventListener('show.bs.modal', function (event) {
+  // Botón que activó el modal
+  var button = event.relatedTarget
+  // Extraer información de los atributos data-bs-*
+  var id = button.getAttribute('data-bs-id')
+  // Si es necesario, puedes iniciar una solicitud AJAX aquí
+  // y luego realiza la actualización en una devolución de llamada.
+  //
+  // Actualizar el contenido del modal.
+  //var modalTitle = eliminarModal.querySelector('.modal-title')
+  var deleteform = eliminarModal.querySelector('#deleteform')
+  var action=deleteform.getAttribute("data-bs-action")
+  deleteform.setAttribute("href",action+id)
+
+  // modalTitle.textContent = 'Nuevo mensaje para ' + recipient
+  // modalBodyInput.value = recipient
+})
+</script>
+
+<!-- fin script -->
--- app/Views/vista.php
+++ app/Views/vista.php
@@ -39,7 +39,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header bg-dark text-light ">
-        <h5 class="modal-title mx-auto">Nuevo mensaje</h5>
+        <h5 class="modal-title mx-auto">Mensaje</h5>
         <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
       </div>
       <div class="modal-body text-center">
