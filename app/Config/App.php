22706b064ca3a5955aa7f7b0797d546e135a9b88
Diego Ubilla <diegoubillaramos@gmail.com>
2022-07-18 14:39:53 -0400

    migraciones y clave foranea agregada

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/PROYECTOWEB/public/';
+    public $baseURL = 'http://localhost/PROYECTOWEBJORGE/public/';
 
     /**
      * --------------------------------------------------------------------------
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'base2',
+        'database' => 'base3',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
